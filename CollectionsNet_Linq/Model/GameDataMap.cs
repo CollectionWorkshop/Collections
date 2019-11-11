81554ef803229f70d0c4640c27a55172ee430743
Łukasz Ksiezak <lukasz.ksiezak@pl.abb.com>
2019-11-11 11:40:31 +0100

    Linq exercises added

---
+++ CollectionsNet_Linq/Model/GameDataMap.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration;
+
+namespace CollectionsNet_Linq.Model
+{
+    public class GameDataMap : ClassMap<GameData>
+    {
+        public GameDataMap()
+        {
+            Map(m => m.Date).Name("Date");
+            Map(m => m.HomeTeam).Name("HomeTeam");
+            Map(m => m.AwayTeam).Name("AwayTeam");
+            Map(m => m.HomeScore).Name("HomeScore");
+            Map(m => m.AwayScore).Name("AwayScore");
+            Map(m => m.Tournament).Name("Tournament");
+            Map(m => m.City).Name("City");
+            Map(m => m.Country).Name("Country");
+            Map(m => m.Neutral).Name("Neutral");
+        }
+    }
+}
---
+++ CollectionsNet_Linq/Model/GameDataProcessor.cs
@@ -0,0 +1,24 @@
+﻿using CsvHelper;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CollectionsNet_Linq.Model
+{
+    public class GameDataProcessor
+    {
+        public IEnumerable<GameData> ProcessData(string pathToData)
+        {
+            using (var reader = new StreamReader(pathToData))
+            {
+                using (var csv = new CsvReader(reader))
+                {
+                    csv.Configuration.Delimiter = ",";
+                    csv.Configuration.RegisterClassMap<GameDataMap>();
+                    var records = csv.GetRecords<GameData>().ToList();
+                    return records;
+                }
+            }
+        }
+    }
+}
