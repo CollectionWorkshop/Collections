f9e03a5a2bad8c75cb297cb61a02ea9918cd764b
Sebastian Krzyzanowicz <sebastian.krzyzanowicz@pl.abb.com>
2019-11-18 13:18:14 +0100

    Moved data to separate project. Introduction of HashSet comparison.

--- CollectionsNet/Program.cs
+++ CollectionsNet/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CollectionsNet_Data;
 
-namespace CollectionsNet_Intro
+namespace CollectionsNet
 {
-    class Program
+    using System;
+
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            //Wczytanie danych z pliku CSV
+            var pathToData = "Project files/results.csv";
+            var dataProcessor = new GameDataProcessor();
+            var data = dataProcessor.ProcessData(pathToData).Select(f => f.HomeTeam).ToArray();
+
+
+            //przerobicze string na klase gameData i odac ovveride GetHashet.
+            HashSetIntro.AddCompare(new List<string>(), new HashSet<string>(), data);
+
+            //var list = new List<string>().AddRange(data);
+            //HashSetIntro.ContainsCompare();
+            Console.ReadKey();
         }
     }
 }
